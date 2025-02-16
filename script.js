ba02d86a65dedb6f4038f44a3b417043b432c160
concasau12 <alldinhnguyen@gmail.com>
2025-02-16 14:13:41 +0700

    change3

--- script.js
+++ script.js
@@ -12,12 +12,44 @@ function clearPassword() {
     document.getElementById("password").value = "";
 }
 
-function checkPassword() {
-    if (enteredPassword === "240903") {
+function checkPassword() { 
+    const CorrectPassword = "240903";
+    
+    if (enteredPassword === CorrectPassword) {
+        document.getElementById("error-message").innerText = "Đúng mật khẩu!";
+
         document.getElementById("password-screen").style.display = "none";
-        document.getElementById("content").style.display = "block";
-    } else {
-        document.getElementById("error-message").innerText = "Mật khẩu sai! Vui lòng thử lại.";
-        clearPassword();
-    }
+       
+        // Hiện phần nội dung sau 5 giây
+        setTimeout(() => {
+            document.getElementById("content").style.display = "block";
+        }, 100);
+
+        setTimeout(() => {
+            window.location.href = "main.html";
+        }, 5000);
+    } else { 
+        document.getElementById("error-message").innerText = "Mật khẩu sai!";
+        
+        // Xóa thông báo sau 2 giây
+        setTimeout(() => { 
+            document.getElementById("error-message").innerText = "";
+        }, 2000);
+    }   
 }
+
+
+
+<script>
+        document.addEventListener("DOMContentLoaded", function () {
+            let container = document.querySelector(".container");
+            function scrollImages() {
+                if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
+                    container.scrollTop = 0;
+                } else {
+                    container.scrollTop += 6;
+                }
+            }
+            setInterval(scrollImages, 30);
+        });
+    </script>
